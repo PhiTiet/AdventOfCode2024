4a5c4a530c603feaf9ee2302ad6d5e57dc2f2308
Phi Tiet <anhphi099@gmail.com>
2024-12-03 18:30:12 +0100

    solves Day3 part2

--- src/main/kotlin/solution/day3/Day3ProblemSolver.kt
+++ src/main/kotlin/solution/day3/Day3ProblemSolver.kt
@@ -7,16 +7,31 @@ class Day3ProblemSolver: AbstractProblemSolver<Int>() {
     private val input = getProblemInput().reduce(String::plus)
 
     override fun partOne(): Int {
-        val reg = Regex("mul\\((?<left>\\d{1,3}),(?<right>\\d{1,3})\\)")
-        return reg.findAll(input)
+        return extractAndMultiplyCommands(input)
+    }
+
+    private fun extractAndMultiplyCommands(command: String) =
+        Regex("mul\\((?<left>\\d{1,3}),(?<right>\\d{1,3})\\)").findAll(command)
             .toList()
             .map { a -> Pair(a.groups["left"]!!.value, a.groups["right"]!!.value) }
             .sumOf { it.first.toInt() * it.second.toInt() }
-    }
 
     override fun partTwo(): Int {
-        val dontRegex = Regex("don't\\(\\)")
-        val doMatcher = Regex("do\\(\\)")
-            return 0
+        val doIndexes = matchIndexes(Regex("do\\(\\)")).toMutableList()
+        val dontIndexes = matchIndexes(Regex("don't\\(\\)")).toMutableList()
+        doIndexes.add(index = 0, element = 0)
+        dontIndexes.add(index = dontIndexes.size, element = input.length - 1)
+        val doRanges = doIndexes.map { it..dontIndexes.smallestValueWhichIsLargerThan(it) }
+        val grouped = doRanges.groupBy { it.last }
+        val unique = grouped.map { a -> a.value.minBy { it.first } }
+        val subStrings = unique.map {  input.substring(it)}
+        val result = subStrings.reduce(String::plus)
+        return extractAndMultiplyCommands(result)
     }
+
+    private fun matchIndexes(regex: Regex): List<Int> {
+        return regex.findAll(input).map { it.range.first }.toList()
+    }
+
+    fun List<Int>.smallestValueWhichIsLargerThan(value: Int) = filter { it > value }.minOrNull()?: size
 }
