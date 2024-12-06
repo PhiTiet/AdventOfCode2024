cdc2601389480c4065f5362f51e1685948ac11cf
Phi Tiet <anhphi099@gmail.com>
2024-12-06 10:19:59 +0100

    adds arrow -> direction and cleans up direction more

--- src/main/kotlin/solution/model/Direction.kt
+++ src/main/kotlin/solution/model/Direction.kt
@@ -3,19 +3,23 @@ package solution.model
 enum class Direction {
     NORTH, EAST, SOUTH, WEST;
 
+    private val arrowSymbols = listOf("^", ">", "v", "<")
 
     fun right(): Direction {
-        val values = values()
-        return values[(ordinal + 1) % values.size]
+        return values()[(ordinal + 1) % values().size]
     }
 
     fun left(): Direction {
-        val values = values()
-        return values[(ordinal + 3) % values.size]
+        return values()[(ordinal + 3) % values().size]
     }
 
     fun reverse(): Direction {
-        val values = values()
-        return values[(ordinal + 2) % values.size]
+        return values()[(ordinal + 2) % values().size]
+    }
+
+    fun arrowSymbolToDirection(symbol: String): Direction {
+        require(arrowSymbols.contains(symbol)) { "$symbol is not a valid direction" }
+        return values()[arrowSymbols.indexOf(symbol)]
+
     }
 }
